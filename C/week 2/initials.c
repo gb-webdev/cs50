04889574325cf9bd296181245dee1c2c9f69a882
Graham <blundell.gp@gmail.com>
2024-01-16 19:13:29 -0800

    added problems from command line lecture/quiz

---
+++ C/week 2/initials.c
@@ -0,0 +1,10 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// Make a function that will return the initials of the string arguments passed into the program
+
+int main(int argc, string argv[])
+{
+    printf("%c\n", argv[1][0]);
+    printf("%c\n", argv[2][0]);
+}
