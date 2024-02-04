78d8758d6f48c4ec87909cc7311415c952688a19
Graham <blundell.gp@gmail.com>
2024-02-03 22:53:14 -0800

    code wars problems

---
+++ C/code-wars/scores.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+// Our team's match results are recorded in a collection of strings.
+// Each match is represented by a string in the format "x:y", where x is our team's score and y is our opponents score.
+
+// For example: ["3:1", "2:2", "0:1", ...]
+
+// Points are awarded for each match as follows:
+
+// if x > y: 3 points (win)
+// if x < y: 0 points (loss)
+// if x = y: 1 point (tie)
+
+// We need to write a function that takes this collection and returns the number of points our team (x) got in the championship by the rules given above.
+
+int points(const char* const games[10]) {
+
+  int result = 0;
+  for (int i = 0; i < 10; i++)
+    {
+      if (games[i][0] > games[i][2])
+        {
+          result += 3;
+        }
+      else if (games[i][0] == games[i][2])
+        {
+          result += 1;        
+        }
+      else if (games[i][0] < games[i][2])
+        {
+          result += 0;
+        }
+    }
+  return result;
+}
