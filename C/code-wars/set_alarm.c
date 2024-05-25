d225e5110a223ceb47c6b87e2da503dae4c699d6
Graham <blundell.gp@gmail.com>
2024-05-24 21:33:39 -0700

    added code wars problems

---
+++ C/code-wars/set_alarm.c
@@ -0,0 +1,25 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+// Write a function named setAlarm/set_alarm/set-alarm/setalarm (depending on language) which receives two parameters. The first parameter, employed, is true whenever you are employed and the second parameter, vacation is true whenever you are on vacation.
+
+// The function should return true if you are employed and not on vacation (because these are the circumstances under which you need to set an alarm). It should return false otherwise. Examples:
+
+// employed | vacation 
+// true     | true     => false
+// true     | false    => true
+// false    | true     => false
+// false    | false    => false
+
+bool set_alarm(bool employed, bool vacation) {
+
+    if (employed == true && vacation == false)
+      {
+      return true;
+      }
+  else
+    {
+    return false;
+    }
+
+}
